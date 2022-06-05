625c2fe5ac42ec0500d81e85a5d5aa8f603f0696
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-05 17:32:41 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#
+# >>> Escriba el codigo del mapper a partir de este punto <<<
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+
+import sys
+
+# itera sobre cada linea de codigo recibida
+# a traves del flujo de entrada
+
+for line in sys.stdin:
+
+# genera las tuplas palabra \tabulador 1
+# ya que es un conteo de palabras
+
+  col = line.split(',')
+  col_credit_history = col[2]
+
+# escribe al flujo estandar de salida
+
+  sys.stdout.write("{}\t1\n".format(col_credit_history))
