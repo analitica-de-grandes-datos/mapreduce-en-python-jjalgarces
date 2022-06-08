be2e901c7f0ddac6c56f128dc3e7ce6072727f04
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-07 22:02:33 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+##FUNCIÓN MAP
+
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+
+import sys
+if __name__ == "__main__":
+
+# itera sobre cada linea de codigo recibida
+# a traves del flujo de entrada
+
+  for line in sys.stdin:
+
+  # genera las tuplas palabra \tabulador 1
+  # ya que es un conteo de palabras
+
+    col = line.split(',')
+    col_purpose = col[3]
+    col_amount = col[4]
+
+  # escribe al flujo estandar de salida
+#     sys.stdout.write("{}\t1\n".format(col_purpose, col_amount))
+#     print(col_purpose, col_amount)
+    print ('%s\t%s' % (col_purpose,col_amount))
