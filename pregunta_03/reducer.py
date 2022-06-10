50a0c284d3449e8b81fb51ef496f9fb0967a6ae4
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-10 15:49:48 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import operator
+if __name__ == '__main__':
+    dict_from_list = {}
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+
+        dict_from_list[key] = val
+
+        sorted_= sorted(dict_from_list.items(), key=operator.itemgetter(1))
+        
+        # sorted_list = sorted(dict_from_list.items(), key=lambda x: x[1])
+        # complete = key + "," + val
+    print(sorted_)
