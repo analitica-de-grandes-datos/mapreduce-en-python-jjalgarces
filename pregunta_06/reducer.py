f76e0bc4034a4dc7d05b6d29deddf8437006c8f3
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-11 20:08:04 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,47 +1,23 @@
-from re import I
 import sys
-#
-# Esta funcion reduce los elementos que tienen la misma clave
-#
+
+
 if __name__ == '__main__':
 
-    curkey = None
-    total = 0
-    Max_6 = 0
-    Min_6 = 0
-    #
-    # cada linea de texto recibida es una entrada clave \tabulador valor
-    #
+    result = {}
+
     for line in sys.stdin:
-        
 
-        key, val = line.split("\t")
-        # val = int(val)
+        clean = line.strip("\n")
+        # line = line.replace("\n", "\t")
+        Col1, Col3 = clean.split("\t")
+        # Col3 = int(Col3)
+        # print(Col1, Col3)
 
-        # print(key, val)
-
-        if key == curkey:
-            #
-            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
-            # clave.
-            #
-            if val > Max_6:
-                Max_6 = val
-            elif val < Min_6:
-                Min_6 = val
-            else:
-                Max_6 = val
-                Min_6 = val
+        if Col1 in result.keys():
+            result[Col1].append(Col3)
         else:
-            #
-            # Se cambio de clave. Se reinicia el acumulador.
-            #
-            if curkey is not None:
+            result[Col1] = [Col3]
 
-                sys.stdout.write("{}\t{}\t{}\n".format(curkey, Max_6, Min_6))
-
-            curkey = key
-            Max_6 = val
-            Min_6 = val
-
-        sys.stdout.write("{}\t{}\t{}\n".format(curkey, Max_6, Min_6))
+    for key, valor in result.items():
+    #     # print(key, float(sum(valor)), sum(valor)/len(valor))
+        sys.stdout.write("{}\t{}\t{}\n".format(key, max(valor), min(valor)))
