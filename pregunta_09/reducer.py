5cfb306dee918c0bcf82c4fd5bed6afe1b8dba88
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-11 20:30:25 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+    tupla_9 = ()
+    Lista_9 = []
+
+    for line in sys.stdin:
+
+        Col1, Col2, Col3 = line.split("\t")
+        Col3 = int(Col3)
+
+        tupla_9 = (Col1, Col2, Col3)
+        # Lista_7 = [Col1, Col2, Col3]
+        Lista_9.append(tupla_9)
+
+
+
+        sorted_= sorted(Lista_9, key=lambda reg: (reg[2]))
+    sort_req = sorted_[:6]
