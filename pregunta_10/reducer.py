ab7a8ff505cfe83e8ddbccb1104b0a02eeba18b6
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-11 22:11:41 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+
+if __name__ == '__main__':
+
+    result = {}
+
+    for line in sys.stdin:
+
+        clean = line.strip("\n")
+
+        Col1, Col2 = clean.split("\t")
+        Col1 = int(Col1)
+        Col2 = Col2.replace(",", "")
+
+        for i in Col2:
+
+            if i in result.keys():
+                result[i].append(Col1)
+            else:
+                result[i] = [Col1]
+
+    for key, valor in result.items():
+        sys.stdout.write("{}\t{}\t\n".format(key, valor))
