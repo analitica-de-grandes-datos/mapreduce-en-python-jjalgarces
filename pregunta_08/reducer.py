a8bb8a804ceeaef4ba961082316a3c70975d49aa
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-11 19:15:19 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
             result[Col1] = [Col3]
 
     for key, valor in result.items():
-        print(key, float(sum(valor)), sum(valor)/len(valor))
+        sys.stdout.write("{} {} {}\n".format(key, float(sum(valor)), sum(valor)/len(valor)))
