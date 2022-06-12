9954cd45d841f71b348399a4853485523a2fc351
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-06-11 21:09:24 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
 
         sorted_= sorted(Lista_9, key=lambda reg: (reg[2]))
     sort_req = sorted_[:6]
-    sys.stdout.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(sort_req[0], sort_req[1], sort_req[2], sort_req[3], sort_req[4], sort_req[5]))
+    sys.stdout.write("{}\n{}\n{}\n{}\n{}\n{}\n".format(sort_req[0], sort_req[1], sort_req[2], sort_req[3], sort_req[4], sort_req[5]))
